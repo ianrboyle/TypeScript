4430edc2d6be0665b0b97df899ddc91ba17f8e35
Ian Boyle <irboyle21@gmail.com>
2022-01-19 09:13:43 -0500

    successfully compiles main.ts

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
-// setting a type to a variable
-var a;
-var b;
-var c;
-var d;
-var e = [1, 2, 3];
-var f = [1, true, "a", false];
-//enum
-var Color;
-(function (Color) {
-  Color[(Color["Red"] = 0)] = "Red";
-  Color[(Color["Green"] = 1)] = "Green";
-  Color[(Color["Blue"] = 2)] = "Blue";
-})(Color || (Color = {}));
-var backgroundColor = Color.Blue;
-var message;
-message = "abc";
-//tells the typescript compiler the type of variable
-var endsWithC = message.endsWith("c");
-var alternativeWat = message.endsWith("c");
+//in line annotation - a tad verbose
+// let drawPoint = (point: { x: number, y: number}) => {
+// }
+// use interfaces instead
+// interfaces - purely for declaration, no implementation
+// interface Point {
+//   x: number,
+//   y: number,
+//   draw: () => void
+// }
+// for cohesion, we need a class, instead of interface
+// allows inclusion of functions, where interfaces do not
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+        console.log("X: " + this.x + ",Y: " + this.y);
+    };
+    Point.prototype.getDistance = function (another) { };
+    return Point;
+}());
+// must initialize Point as a new operator
+var point = new Point;
+point.x = 1;
+point.y = 2;
+point.draw();
--- main.ts
+++ main.ts
@@ -18,12 +18,12 @@ class Point {
   x: number;
   y: number;
   draw() {
-
+    console.log("X: " + this.x + ", Y: " + this.y)
   }
   getDistance(another: Point) {}
 }
-
-let drawPoint = (point: Point) => {
-
-}
-
+// must initialize Point as a new operator
+let point = new Point;
+point.x = 1;
+point.y = 2;
+point.draw();
