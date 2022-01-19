9b7de8f70db1d84a8bfc89b83d04666891341d97
Ian Boyle <irboyle21@gmail.com>
2022-01-19 08:54:20 -0500

    adds compiled tsc

---
+++ main.js
@@ -0,0 +1,15 @@
+// setting a type to a variable
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+//enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var backgroundColor = Color.Blue;
