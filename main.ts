d18cabb1dfa2cea5ff67fc821158e03e9577cf79
Ian Boyle <irboyle21@gmail.com>
2022-01-19 08:53:44 -0500

    adds main.ts - explores setting a type to varaible

---
+++ main.ts
@@ -0,0 +1,12 @@
+// setting a type to a variable
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3]
+let f: any[] = [1, true, 'a', false]
+
+
+//enum
+enum Color { Red = 0, Green = 1, Blue = 2}
+let backgroundColor = Color.Blue;
